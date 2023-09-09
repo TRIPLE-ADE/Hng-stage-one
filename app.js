c6028e42f5bfd4de638d5329df3abd69379b3c6f
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-09-09 16:53:42 +0100

    Update JavaScript day and time function

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
-    // Select the HTML elements with specific data-testid attributes
-    const currentDayofWeekElm = document.querySelector('[data-testid="currentDayofWeek"]');
-    const currentUTCTimeElm = document.querySelector('[data-testid="currentUTCTime"]');
+// Select the HTML elements with specific data-testid attributes
+const currentDayofWeekElm = document.querySelector(
+  '[data-testid="currentDayofWeek"]'
+);
+const currentUTCTimeElm = document.querySelector(
+  '[data-testid="currentUTCTime"]'
+);
 
-    // Get the current day of the week and the current time in UTC
+// function to update the current UTC time
+function updateCurrentUTCTime() {
+  // Create a new Date object to get the current UTC time
+  const currentUTCTime = new Date().getTime();
 
-    // Get the current day of the week (0 for Sunday, 1 for Monday, etc.)
-    const currentDay = new Date().getDay();
+  // Set the innerHTML of an element to display the current UTC time
+  currentUTCTimeElm.innerHTML = currentUTCTime;
+}
 
-    // Define an array of day names
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    // corresponding to the current day
-    const currentDayofWeek = days[currentDay];
+// function to update the current UTC time
+function updateCurrentDayofWeek() {
+  // Get the current day of the week (0 for Sunday, 1 for Monday, etc.)
+  const currentDay = new Date().getDay();
 
-    // Set the innerHTML of the currentDayofWeek element to display the current day of the week
-    currentDayofWeekElm.innerHTML = currentDayofWeek;
+  // Define an array of day names
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    // function to update the current UTC time
-    function updateCurrentUTCTime() {
-        // Create a new Date object to get the current UTC time
-        const currentUTCTime = new Date().getTime();
-      
-        // Set the innerHTML of an element to display the current UTC time
-        document.querySelector('[data-testid="currentUTCTime"]').innerHTML = currentUTCTime;
-      }
-    //calling function 
-      updateCurrentUTCTime();
-      
-      // Update the time every second (1000 milliseconds)
-      setInterval(updateCurrentUTCTime, 1000);
-      
+  // corresponding to the current day
+  const currentDayofWeek = days[currentDay];
+
+  // Set the innerHTML of the currentDayofWeek element to display the current day of the week
+  currentDayofWeekElm.innerHTML = currentDayofWeek;
+}
+
+// function to call updateCurrentDayofWeek and updateCurrentUTCTime
+function updateDayAndTime() {
+    //calling function to update the current day of the week
+    updateCurrentDayofWeek();
+
+    //calling function to updtate the current UTC time
+    updateCurrentUTCTime();
+}
+
+// Update the time every second (1000 milliseconds)
+setInterval(updateDayAndTime, 1000);
